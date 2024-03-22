f8163b98bd8164e9cf479331b7d27d31751bfdde
Adam Jarzabek <ajarzabe@unitygroup.com>
2024-03-22 11:42:33 +0100

    Initial commit

---
+++ src/main/java/com/capitolmanager/CapitolManagerApplication.java
@@ -0,0 +1,13 @@
+package com.capitolmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitolManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitolManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitolmanager/CapitolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitolmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitolManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
